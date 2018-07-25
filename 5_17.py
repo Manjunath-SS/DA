259c1596be89bafa888d97d91f5ff0e1870b44bf
Manjunath Sangashetty <mssangashetty@gmail.com>
2018-07-25 18:45:50 +0530

    ReadingReverseLogic

--- 5_17.py
+++ 5_17.py
@@ -1,9 +1,20 @@
-import os
 try:
-    f=open("small.txt","r")
+    f=open("small.txt","rb+")
+    f.seek(0,2)
+    l=f.tell()
+    st=''
+    for i in range(-1,-l-1,-1):
+        f.seek(i,2)
+        st+=str(f.read(1).decode())
+    print(st)
+    
+   
+        
+   
+    '''f=open("small.txt","r")
     con=f.read()
     for i in reversed(con):
-        print(i,end='')
+        print(i,end='')'''
     f.close()
     
 except:
