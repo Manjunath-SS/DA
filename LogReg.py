f924c184401df5a94cfc157960be30a370ce0f82
Manjunath Sangashetty <mssangashetty@gmail.com>
2018-08-08 12:42:24 +0530

    LogRegGraph

--- LogReg.py
+++ LogReg.py
@@ -9,14 +9,26 @@ from sklearn.cross_validation import train_test_split
 
 df=pd.read_csv('adult.csv')
 
-df['income']=df['income'].map({'<=50K':-1, '>50K':1})
+df['income']=df['income'].map({'<=50K':0, '>50K':1})
 
 X=df[['capital-gain','capital-loss','age','fnlwgt','educational-num','hours-per-week']]
 y=df['income']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
 
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
